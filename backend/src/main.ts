a90a1a0c96e10d3e4171cfe03aadac8639b5f9b1
Humberto Freitas <jhcff88@gmail.com>
2026-06-11 00:16:31 -0300

    feat: Swagger Adicionado

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,6 +20,16 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  // --- CONFIGURAÇÃO DA PÁGINA VISUAL DO SWAGGER ---
+  const config = new DocumentBuilder()
+    .setTitle('API ACIC')
+    .setDescription('Documentação dos endpoints do painel administrativo ACIC')
+    .setVersion('1.0')
+    .addBearerAuth() // Deixa pronto o campo de Token JWT para o login do Cainã
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
